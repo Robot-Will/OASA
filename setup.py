3f4a16a1fab1427b9a90509ee3a1f295c0e92ed3
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-07-21 23:01:50 +0200

    modern packaging

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
-#! /usr/bin/python
+from setuptools import setup
 
-import sys
-from distutils.core import setup
-import glob
-import os
-import operator
-
-# the setup itself
+setup()
 
 set = setup(
-  name = 'oasa',
-  version = '0.13.1',
-  description = "OASA is a free cheminformatics library written in Python",
-  author = "Beda Kosata",
-  author_email = "beda@zirael.org",
-  url = "http://bkchem.zirael.org/oasa_en.html",
-  license = "GNU GPL",
-  platforms = ["Unix", "Windows", "hopefully other OSes able to run Python"],
-  long_description = "OASA is a free cheminformatics library written in Python",
-  
-  packages=[ 'oasa', 'oasa/graph'],
+    name='oasa',
+    version='0.13.1',
+    description="OASA is a free cheminformatics library written in Python",
+    author="Beda Kosata",
+    author_email="beda@zirael.org",
+    url="http://bkchem.zirael.org/oasa_en.html",
+    license="GNU GPL",
+    platforms=["Unix", "Windows", "hopefully other OSes able to run Python"],
+    long_description="OASA is a free cheminformatics library written in Python",
 
-  #data_files=[ ('oasa', glob.glob( 'oasa/templates/*.cdml')+glob.glob('templates/*.xml')),
+    packages=['oasa', 'oasa/graph'],
 
-  #windows=['oasa'],
-  #options = {"py2exe": {"packages": ["encodings"]}}
-  )
+    # data_files=[ ('oasa', glob.glob( 'oasa/templates/*.cdml')+glob.glob('templates/*.xml')),
 
+    # windows=['oasa'],
+    # options = {"py2exe": {"packages": ["encodings"]}}
+)
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+from __future__ import print_function
 import oasa
 #print oasa.CAIRO_AVAILABLE
 
@@ -5,7 +7,7 @@ def cairo_out_test2():
     mol = oasa.smiles.text_to_mol( "c1ccccc1Cl.c1ccccc1OC.CCCl")
     mol.normalize_bond_length( 30)
     mol.remove_unimportant_hydrogens()
-    c = oasa.cairo_out.cairo_out( color_bonds=True, color_atoms=True)
+    c = oasa.cairo_out.cairo_out(color_bonds=True, color_atoms=True)
     c.show_hydrogens_on_hetero = True
     c.font_size = 20
     mols = list( mol.get_disconnected_subgraphs())
@@ -14,8 +16,8 @@ def cairo_out_test2():
     c.mols_to_cairo( mols, "test.svg", format="svg")
 
 def inchi_test():
-    mol = oasa.smiles.text_to_mol( "c1ccccc1\C=C/CC")
-    print oasa.inchi.mol_to_text( mol, program="stdinchi-1.exe", fixed_hs=False)
+    mol = oasa.smiles.text_to_mol( r"c1ccccc1\C=C/CC")
+    print(oasa.inchi.mol_to_text( mol, program="stdinchi-1.exe", fixed_hs=False))
 
 cairo_out_test2()
 #inchi_test()
